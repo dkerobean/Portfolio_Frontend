e8b4f4d68a59dba557f43a6c40fb07330c9b850d
Dickson Amenyo <36271402+dkerobean@users.noreply.github.com>
2024-08-19 10:15:19 +0000

    buh fixes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c3f1832ec2d2b1f"; }
+/******/ 		__webpack_require__.h = function() { return "b0b38723e812acea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/work-single.jsx
+++ pages/work-single.jsx
@@ -206,25 +206,7 @@ const WorkSingle = () => {
       </section> */}
       {/* Section - Video */}
 
-      {project.link2 && (
-        <div className="section section-inner m-video-large">
-          <div className={`video ${videoToggle ? "active" : ""}`}>
-            <div
-              className="img js-parallax"
-              style={{ backgroundImage: `url(${project.image2})` }}
-            />
-            {/* Embed YouTube video using react-youtube */}
-            <YouTube
-              videoId={getYouTubeVideoId(project.link2)}
-              opts={{ width: '100%', height: '100%' }}
-              onReady={(event) => {
-                // You can perform actions when the video player is ready
-              }}
-            />
-            <div className="play" onClick={() => setVideoToggle(true)} />
-          </div>
-        </div>
-      )}
+      
 
 
       {/* Section - Navigation */}
